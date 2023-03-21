34da70e863b6b1941a26ace6a1f9985048de95ed
manurodriguez <manurodriguez@itba.edu.ar>
2023-03-21 10:50:01 -0300

    :sparkles: color and typography

---
+++ lib/tokens/colors.dart
@@ -0,0 +1,32 @@
+//  ..................................................
+//  .██████╗.██████╗.██╗......██████╗.██████╗.███████╗
+//  ██╔════╝██╔═══██╗██║.....██╔═══██╗██╔══██╗██╔════╝
+//  ██║.....██║...██║██║.....██║...██║██████╔╝███████╗
+//  ██║.....██║...██║██║.....██║...██║██╔══██╗╚════██║
+//  ╚██████╗╚██████╔╝███████╗╚██████╔╝██║..██║███████║
+//  .╚═════╝.╚═════╝.╚══════╝.╚═════╝.╚═╝..╚═╝╚══════╝
+//  ..................................................
+
+
+import 'package:flutter/material.dart';
+
+class Group7Colors {
+  Group7Colors._();
+
+  static const Color neutral0 = Color(0xFFFFFFFF);
+  static const Color neutral10 = Color(0xFFFAFAFA);
+  static const Color neutral25 = Color(0xFFE0E0E0);
+  static const Color neutral50 = Color(0xFF9E9E9E);
+  static const Color neutral75 = Color(0xFF666666);
+  static const Color neutral100 = Color(0xFFFFFFFF);
+  
+  static const Color primary100 = Color(0xFF14903F);
+  
+  static const Color secondary50 = Color(0xFF90CAF9);
+  static const Color secondary80 = Color(0xFF6CB6F3);
+  static const Color secondary90 = Color(0xFF4AA9F5);
+  static const Color secondary100 = Color(0xFF2C98F0);
+  static const Color secondary200 = Color(0xFF0D47A1);
+
+  static const Color error100 = Color(0xFFB3261E);
+}
---
+++ lib/tokens/typography.dart
@@ -0,0 +1,17 @@
+class Group7Typography {
+  Group7Typography._();
+
+  //fonts
+  static const String family = 'Roboto';
+
+  static const double fontSizeHeading1 = 24;
+
+  static const double fontSizeHeading2 = 20;
+  static const double fontSizeSubtitle1 = 16;
+  static const double fontSizeBody1 = 14;
+  static const double fontSizeBody2 = 12;
+  static const double fontSizeButton = 14;
+  static const double fontSizeCaption = 12;
+  static const double fontSizeOverline = 10;
+}
+
