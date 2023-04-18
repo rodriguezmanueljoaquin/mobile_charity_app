57ec01aebb573d2c732d0ef07799c00ff1bdd33b
octavio serpe <octavio.serpe@gmail.com>
2023-04-18 20:02:57 -0300

    :lipstick: change md space

--- lib/design_system/tokens/spacing.dart
+++ lib/design_system/tokens/spacing.dart
@@ -6,7 +6,7 @@ class SerManosSpacing {
   static const double spaceXS = 5;
   static const double spaceSM = 8;
   static const double spaceSL = 13;
-  static const double spaceMD = 21;
+  static const double spaceMD = 24;
   static const double spaceLG = 34;
   static const double spaceXL = 55;
   static const double spaceXXL = 89;
