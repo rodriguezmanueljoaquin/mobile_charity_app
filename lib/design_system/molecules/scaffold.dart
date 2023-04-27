a986ce0a83a1269d44996773813ffd315662ffea
octavio serpe <octavio.serpe@gmail.com>
2023-04-27 15:20:22 -0300

    :lipstick: add padding boolean to scaffold

--- lib/design_system/molecules/scaffold.dart
+++ lib/design_system/molecules/scaffold.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_charity_app/design_system/tokens/colors.dart';
+import 'package:mobile_charity_app/design_system/tokens/spacing.dart';
 
 class SerManosScaffold extends StatelessWidget {
   final Widget body;
+  final bool applyPadding;
 
-  const SerManosScaffold({super.key, required this.body});
+  const SerManosScaffold({
+    super.key,
+    required this.body,
+    this.applyPadding = true,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 0,
-        systemOverlayStyle: const SystemUiOverlayStyle(statusBarColor: SerManosColors.secondary90,)
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: SerManosColors.secondary90,
+        ),
       ),
       body: SizedBox(
         width: double.infinity,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          padding: EdgeInsets.symmetric(
+              horizontal: applyPadding ? SerManosSpacing.spaceSL : 0),
           child: body,
         ),
       ),
