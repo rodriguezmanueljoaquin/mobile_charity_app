1d171cec8af0dd2854d7cb82a03892b0a3690919
manurodriguez <manurodriguez@itba.edu.ar>
2023-04-29 15:20:09 -0300

    :sparkles: tabs in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mobile_charity_app/design_system/atoms/logos.dart';
 import 'package:mobile_charity_app/design_system/atoms/sized_box.dart';
 import 'package:mobile_charity_app/design_system/molecules/inputs.dart';
 import 'package:mobile_charity_app/design_system/organisms/cards/news_card.dart';
 import 'package:mobile_charity_app/design_system/organisms/cards/volunteering_card.dart';
+import 'package:mobile_charity_app/design_system/tokens/colors.dart';
 import 'package:mobile_charity_app/design_system/tokens/spacing.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,43 +23,45 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(89.0),
-            child: AppBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [SerManosLogos.appBar, const Spacer()],
-              ),
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(50.0),
-                child: ColoredBox(
-                  color: Colors.blue[300]!,
-                  child: TabBar(
-                    unselectedLabelColor: Color.fromARGB(255, 216, 244, 54),
-                    indicator: BoxDecoration(
-                      color: Colors.blue[500]!,
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.white, // Color for the indicator line
-                          width: 2.0, // Thickness of the indicator line
-                        ),
-                      ),
+        appBar: AppBar(
+          backgroundColor: SerManosColors.secondary90,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: SerManosColors.secondary90,
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [SerManosLogos.appBar, const Spacer()],
+          ),
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(50.0),
+            child: ColoredBox(
+              color: SerManosColors.secondary100,
+              child: TabBar(
+                unselectedLabelColor: SerManosColors.neutral0,
+                indicator: BoxDecoration(
+                  color: SerManosColors.secondary200,
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.white, // Color for the indicator line
+                      width: 3.0, // Thickness of the indicator line
                     ),
-                    tabs: [
-                      Tab(
-                        text: 'POSTULARSE',
-                      ),
-                      Tab(
-                        text: 'MI PERFIL',
-                      ),
-                      Tab(
-                        text: 'NOVEDADES',
-                      ),
-                    ],
                   ),
                 ),
+                tabs: [
+                  Tab(
+                    text: 'POSTULARSE',
+                  ),
+                  Tab(
+                    text: 'MI PERFIL',
+                  ),
+                  Tab(
+                    text: 'NOVEDADES',
+                  ),
+                ],
               ),
-            )),
+            ),
+          ),
+        ),
         body: TabBarView(
           children: [
             ListView.separated(
