762918ef486006c5b754a58cc3c3041fdd1ef3e1
manurodriguez <manurodriguez@itba.edu.ar>
2023-05-01 19:50:21 -0300

    :bug: fix encode in VolunteeringModel

--- lib/models/volunteering.dart
+++ lib/models/volunteering.dart
@@ -8,4 +8,20 @@ class VolunteeringModel {
     required this.title,
     required this.description,
   });
+
+  factory VolunteeringModel.fromJson(Map<String, dynamic> json) {
+    return VolunteeringModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+    };
+  }
 }
