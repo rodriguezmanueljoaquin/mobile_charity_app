bb313ee8c4497b1b611d00ce4dcf2d9d3684d286
octavio serpe <octavio.serpe@gmail.com>
2023-06-19 21:45:41 -0300

    :sparkles: add empty news placeholder

--- lib/pages/home/tabs/news.dart
+++ lib/pages/home/tabs/news.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_charity_app/design_system/atoms/sized_box.dart';
+import 'package:mobile_charity_app/design_system/organisms/cards/empty_list_placeholder_card.dart';
 import 'package:mobile_charity_app/design_system/organisms/cards/news_card.dart';
 import 'package:mobile_charity_app/design_system/tokens/indicators.dart';
 import 'package:mobile_charity_app/design_system/tokens/spacing.dart';
+import 'package:mobile_charity_app/models/news.dart';
 import 'package:mobile_charity_app/providers/news_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -30,23 +32,44 @@ class _NewsTabState extends State<NewsTab>
   @override
   Widget build(BuildContext context) {
     return SerManosRefreshIndicator(
-        onRefresh: Provider.of<NewsProvider>(context, listen: false).fetchNews,
-        child: Consumer<NewsProvider>(
-            builder: (context, newsProvider, child) => newsProvider.isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : ListView.separated(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: SerManosSpacing.spaceSL,
-                      vertical: SerManosSpacing.spaceLG,
-                    ),
-                    itemBuilder: (context, index) => UnconstrainedBox(
-                      child: SerManosNewsCard(
-                        news: newsProvider.news![index],
-                      ),
-                    ),
-                    separatorBuilder: (context, index) =>
-                        const SerManosSizedBox.md(),
-                    itemCount: newsProvider.news!.length,
-                  )));
+      onRefresh: Provider.of<NewsProvider>(context, listen: false).fetchNews,
+      child: Consumer<NewsProvider>(builder: (context, newsProvider, child) {
+        if (newsProvider.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        List<NewsModel>? news = newsProvider.news;
+
+        if (news == null || news.isEmpty) {
+          return const Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: SerManosSpacing.spaceSL,
+                vertical: SerManosSpacing.spaceMD,
+              ),
+              child: EmptyListPlaceholderCard(
+                text:
+                    "Actualmente no hay noticias vigentes. Pronto se irán ircorporando nuevas.",
+              ),
+            ),
+          );
+        }
+
+        return ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            horizontal: SerManosSpacing.spaceSL,
+            vertical: SerManosSpacing.spaceLG,
+          ),
+          itemBuilder: (context, index) => UnconstrainedBox(
+            child: SerManosNewsCard(
+              news: news[index],
+            ),
+          ),
+          separatorBuilder: (context, index) => const SerManosSizedBox.md(),
+          itemCount: news.length,
+        );
+      }),
+    );
   }
 }
